6d38acdef421c38d9e6b3bb9b1b14fd528fcc9e5
SamuelSeyi <steadywords@gmail.com>
2024-03-15 14:32:12 -0700

    First Commmit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+for (i=99; i>=1; i--){
+  console.log(`${i} Packets Of Biscuits On The Wall, ${i}Packets Of Biscuits / Take one down and pass it around, ${i-1} bottles of beer on the wall.`);
+
+
+  lyrics.innerHTML +=  `${i} Packets Of Biscuits On The Wall, ${i} Packets Of Biscuits <br> Take one down and pass it around, ${i-1} Packets Of Biscuits on the wall. <br><br><br>`
+}
+
+
